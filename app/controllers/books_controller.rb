57fff0b2153482b293500659bff620ef7eb738d3
siragasnow <siraga613@gmail.com>
2020-06-02 14:54:19 +0900

    [add]4-8 top,index,new function

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  	@book = Book.new
+  end
+
+  def show
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book.id)
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
